01b5e650628aac333bb37d439cb3a29265cbe1ca
School <kaboy@icloud.com>
2022-01-20 23:29:50 -0600

    retrieve all thoughts and users

--- server.js
+++ server.js
@@ -17,3 +17,34 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populatedb', {
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('debug', true);
+
+// Demo user
+db.User.create({ name: 'John Doe' })
+  .then(dbUser => {
+    console.log(dbUser);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+// Retrieve all thoughts
+app.get('/thoughts', (req, res) => {
+  db.Note.find({})
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// Retrieve all users
+app.get('/user', (req, res) => {
+  db.User.find({})
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
