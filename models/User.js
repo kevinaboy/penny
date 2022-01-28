2fd7037b2e4aeb254bb0e86a2156899bfcad965b
School <kaboy@icloud.com>
2022-01-27 20:04:33 -0600

    updated User model with username, email, thoughts, and friends

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+  },
+  thoughts: [{
+    type: Schema.Types.ObjectId,
+    ref: ''
+  }],
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
